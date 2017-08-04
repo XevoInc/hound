8c8a91ab154e1bd9452a0854a5bfec3d82edeafd
Martin Kelly <mkelly@xevo.com>
2017-08-04 15:03:38 -0700

    cleanly build with clang, std=c11

--- src/core/assert.c
+++ src/core/assert.c
@@ -15,12 +15,14 @@
 #define MAX_EXPR_STR_LEN (100)
 #define MAX_ARG_STR_LEN (1000)
 
+#define MAX_FORMAT_STR_LEN  ( \
+    STATIC_STRLEN(_HOUND_ASSERT_STR_BASE) + \
+    MAX_EXPR_STR_LEN + \
+    STATIC_STRLEN(s_loc_str) + \
+    MAX_ARG_STR_LEN \
+    )
+
 static const char s_loc_str[] = _HOUND_ASSERT_STR_LOC_DETAILS "\n";
-static const size_t s_max_str_len =
-    STATIC_STRLEN(_HOUND_ASSERT_STR_BASE) +
-    MAX_EXPR_STR_LEN +
-    STATIC_STRLEN(s_loc_str) +
-    MAX_ARG_STR_LEN;
 
 void _assert_log_msg(
     const char *expr,
@@ -30,7 +32,7 @@ void _assert_log_msg(
     const char *fmt,
     va_list args)
 {
-    char msg[s_max_str_len];
+    char msg[MAX_FORMAT_STR_LEN];
     char *pos;
 
     /*
@@ -39,7 +41,15 @@ void _assert_log_msg(
      */
     pos = msg;
     pos += sprintf(pos, _HOUND_ASSERT_STR_BASE);
+    /*
+     * Override the -Wformat-security warning here regarding snprintf on
+     * untrusted input because expr comes from a limited set of compile-time
+     * assert macros, which are all safe.
+     */
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wformat-security"
     pos += snprintf(pos, MAX_EXPR_STR_LEN, expr);
+    #pragma GCC diagnostic pop
     pos += sprintf(pos, s_loc_str, file, line, func);
     vsnprintf(pos, MAX_ARG_STR_LEN, fmt, args);
 
